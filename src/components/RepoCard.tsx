e3d5964ee0cc2db85f2504b6f236f8e1afff8626
Samwul <datong.samwul@gmail.com>
2021-12-25 13:18:43 +0100

    make card more presentable

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,12 +1,16 @@
-import { Link } from "react-router-dom"
+import { Button } from "@chakra-ui/react"
+import { Link, useNavigate } from "react-router-dom"
+import GitIconSvg from '../assets/git-icon.svg'
 import "../css/RepoCard.css"
 
 export const RepoCard: React.FC<{name: string, url: string, [key: string]: any}> = ({name, url, ...pros}) => {
 
+    const navigate = useNavigate()
     return (
         <div className="repo-card">
-            <h4>Repo name: {name}</h4>
-            <p>Link: <Link to={url}>{url} </Link> </p>
+            <img src={GitIconSvg} width={'70px'}/>
+            <h4 style={{minWidth: '300px'}}>{name}</h4>
+            <Button className="repo-btn" onClick={() => {navigate(url)}}> View Read me </Button>
         </div>
     )
 }
